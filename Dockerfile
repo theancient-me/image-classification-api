081a40a883612e0b67e2a2a85afd1184be25fdc6
jatawatsafe <jatawatsafe@hotmail.com>
2021-05-18 17:51:17 +0700

    [ADD] MNIST model

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM python:3.7
+FROM python:3.9.1
 
-RUN pip install fastapi uvicorn
+COPY . /app
+WORKDIR /app
+RUN pip install -r app/requirements.txt
 
 EXPOSE 80
 
 COPY ./app /app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -1,15 +1,35 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from app.mnist import PredictImage
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+from PIL import Image
+from io import BytesIO
+import numpy as np
 
 app = FastAPI()
+model = PredictImage("./app/my_model.h5")
 
+@app.post("/files")
+async def create_file(file: bytes = File(...)):
+    stream = BytesIO(file)
+    image = np.array(Image.open(stream))
+    stream.close()
+    image = 255 - image
+    # print(type(image))
+    # print(image.shape)
+    print(model.predict(image))
+    val_predict = int(model.predict(image))
+    return {"Predict": val_predict}
 
+# for mockup
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def main():
+    content = """
+    <body>
+    <form action="/files" enctype="multipart/form-data" method="post">
+    <input name="files" type="file" multiple>
+    <input type="submit">
+    </form>
+    </body>
+    """
+    return HTMLResponse(content=content)
